0d8c0ae23956b86d205f9272647756e18f337e87
VarunSaini02 <varun.saini@gmail.com>
2020-05-05 13:08:29 -0500

    Add print statement: hello world!

--- BattleOfWill/GameSettingsViewController.swift
+++ BattleOfWill/GameSettingsViewController.swift
@@ -14,6 +14,7 @@ class GameSettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello world!")
         // Do any additional setup after loading the view.
     }
 
